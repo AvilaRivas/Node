59a17a7883d72aa2cd9b029a8bac93d6cafb589c
Victor Avila <dev_game@outlook.com>
2020-08-04 16:24:17 -0500

    full clima mundo

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+const api = require('./weather/api');
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -8,3 +10,24 @@ const argv = require('yargs').options({
 
 
 console.log(argv.direccion);
+
+let city = argv.direccion;
+
+api.getWeather(city).then(data => {
+    if (data.data.cod == 200) {
+        console.log(`El clima de ${loc} es de ${data.data.main.temp}`);
+    }
+})
+.catch(err => {
+    console.log(`Error message: `, err);
+})
+
+
+// let trest = async() => {
+//     try {
+//         let test1 = await api.getWeather(city);
+//     }
+//     catch {
+
+//     }
+// }
