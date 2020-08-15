1ead410c9ae770c1912467e091df2f28e8aac759
Victor Avila <dev__game@outlook.com>
2020-08-15 13:38:23 -0500

    just in case

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express();
 const hbs = require('hbs');
+require('./hbs/helpers')
 
 app.use(express.static(__dirname + '/public'));
 
 //Exrpress HBS engine
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs'); 
+
+
+
 app.get('/', (req, res) => {
     res.render('home', {
         nombre: 'Victor',
